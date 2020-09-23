a0bd2a57d2073dc13a28f209a877f9db24f67313
Mike <mshiznitzh@gmail.com>
2020-09-23 09:40:01 -0500

    Change function name in main

--- Pete_Maintenace_Helper.py
+++ Pete_Maintenace_Helper.py
@@ -321,7 +321,7 @@ def Create_task_for_ESID_before_Energiztion(scheduledf):
         Add_Task(description, project, duedate, priority, 'PMH')
 
 
-def Create_task_for_Engineering_Activities(scheduledf):
+def Create_tasks_for_Engineering_Activities(scheduledf):
 
     #This code filters out the start dates for TE activities and creates tasks
     EDdf = scheduledf[(scheduledf['Grandchild'] == 'Electrical Design') &
@@ -1186,7 +1186,7 @@ def main():
         # Create_task_for_Final_Engineering_with_draft_schedules(myprojectsdf, scheduledf)
         #Create_task_for_Released_projects_missing_Construnction_Ready_Date(Project_Schedules_All_Data_df)
         Create_task_for_ESID_before_Energiztion(Project_Schedules_All_Data_df)
-        Create_task_for_Electrical_Activities(Project_Schedules_All_Data_df)
+        Create_tasks_for_Engineering_Activities
         Create_task_for_Relay_Settings(Project_Schedules_All_Data_df)
         Create_task_for_add_WA_to_schedule(Project_Schedules_All_Data_df, myprojectbudgetitmes)
         Create_tasks_for_Waterfalls(Project_Schedules_All_Data_df)
